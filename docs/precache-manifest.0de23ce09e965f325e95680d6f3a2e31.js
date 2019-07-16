c77010f63cbd6190ccdf8d688684b3e41b5c351a
Chahana Tyagi <chahana.tyagi@tokopedia.com>
2019-07-17 02:04:58 +0530

    Project home page added

---
+++ docs/precache-manifest.0de23ce09e965f325e95680d6f3a2e31.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef3380b671a8b2d4b842",
+    "url": "/myportfolio/static/css/main.c5cb0850.chunk.css"
+  },
+  {
+    "revision": "ef3380b671a8b2d4b842",
+    "url": "/myportfolio/static/js/main.ef3380b6.chunk.js"
+  },
+  {
+    "revision": "b84e791988842ae2205f",
+    "url": "/myportfolio/static/js/1.b84e7919.chunk.js"
+  },
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "4ce4dde0137066afb6f9b421b2e6c1ae",
+    "url": "/myportfolio/static/media/reactIcon.4ce4dde0.jpg"
+  },
+  {
+    "revision": "fcf40d6a94df7a5a340cc3d9e9f1aca1",
+    "url": "/myportfolio/static/media/sassIcon.fcf40d6a.png"
+  },
+  {
+    "revision": "ac50dac8f518dee6249ef3334d70646c",
+    "url": "/myportfolio/static/media/profile.ac50dac8.jpg"
+  },
+  {
+    "revision": "29362830b8fa50a611e511609ec30e99",
+    "url": "/myportfolio/static/media/stb.29362830.jpeg"
+  },
+  {
+    "revision": "0cc50bb911e6bc93d0561ce504ebcc8d",
+    "url": "/myportfolio/static/media/gmail-logo.0cc50bb9.svg"
+  },
+  {
+    "revision": "346a2d2b6410054c6e686b2164e3a0c7",
+    "url": "/myportfolio/static/media/medium.346a2d2b.svg"
+  },
+  {
+    "revision": "3bd240d1095303cbec81012dd0529638",
+    "url": "/myportfolio/static/media/github-logo.3bd240d1.svg"
+  },
+  {
+    "revision": "3ee056b2b00773d600d675693e651784",
+    "url": "/myportfolio/static/media/ubiquity.3ee056b2.png"
+  },
+  {
+    "revision": "ceac24b66efc762db21e8e95453e3476",
+    "url": "/myportfolio/static/media/pinddy.ceac24b6.jpeg"
+  },
+  {
+    "revision": "3ac7f79ce29bd38726cf082b0882c536",
+    "url": "/myportfolio/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.324f0c0d76cfc3d76288123ab1969d77.js"
+  "/myportfolio/precache-manifest.0de23ce09e965f325e95680d6f3a2e31.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/myportfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
