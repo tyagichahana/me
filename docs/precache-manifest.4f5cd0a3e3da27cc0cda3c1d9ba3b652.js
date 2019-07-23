11ce072c6f699b69284af26e37bb0b803750ecfc
Chahana Tyagi <chahana.tyagi@tokopedia.com>
2019-07-23 20:11:06 +0530

    build

---
+++ docs/precache-manifest.4f5cd0a3e3da27cc0cda3c1d9ba3b652.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "80b045c3367a0c283750",
+    "url": "/static/css/main.2c20ff8b.chunk.css"
+  },
+  {
+    "revision": "80b045c3367a0c283750",
+    "url": "/static/js/main.80b045c3.chunk.js"
+  },
+  {
+    "revision": "6ae50b71493670deb877",
+    "url": "/static/js/1.6ae50b71.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ce4dde0137066afb6f9b421b2e6c1ae",
+    "url": "/../docs/media/reactIcon.4ce4dde0.jpg"
+  },
+  {
+    "revision": "fcf40d6a94df7a5a340cc3d9e9f1aca1",
+    "url": "/../docs/media/sassIcon.fcf40d6a.png"
+  },
+  {
+    "revision": "ac50dac8f518dee6249ef3334d70646c",
+    "url": "/../docs/media/profile.ac50dac8.jpg"
+  },
+  {
+    "revision": "29362830b8fa50a611e511609ec30e99",
+    "url": "/../docs/media/stb.29362830.jpeg"
+  },
+  {
+    "revision": "dcae6c699d6d9168c6372acf08587a1e",
+    "url": "/../docs/media/Chahana-Resume.dcae6c69.pdf"
+  },
+  {
+    "revision": "3ee056b2b00773d600d675693e651784",
+    "url": "/../docs/media/ubiquity.3ee056b2.png"
+  },
+  {
+    "revision": "ceac24b66efc762db21e8e95453e3476",
+    "url": "/../docs/media/pinddy.ceac24b6.jpeg"
+  },
+  {
+    "revision": "99fbff33b74d31be82d52c19b87c400c",
+    "url": "/index.html"
+  }
+];
